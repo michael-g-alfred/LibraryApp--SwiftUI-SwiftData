1807cfd339ba4f12c66269aca9f702fde4efc437
michael-g-alfred <Michoolgeorge@gmail.com>
2026-01-22 21:18:39 +0200

    Add book count sorting and fix cover display

---
+++ LibraryApp--SwiftUI+SwiftData/Views/AuthorListView.swift
@@ -0,0 +1,18 @@
+//
+//  AuthorListView.swift
+//  LibraryApp--SwiftUI+SwiftData
+//
+//  Created by Michael George on 22/01/2026.
+//
+
+import SwiftUI
+
+struct AuthorListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AuthorListView()
+}
